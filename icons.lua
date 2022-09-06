8ded49d94f55e34acf8430ca34614936fe2eba09
Lucas <90154527+Lucasfin000@users.noreply.github.com>
2022-09-05 22:31:24 -0400

    added icons

---
+++ icons.lua
@@ -0,0 +1,940 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Space Hub Icons Kit", HidePremium = false, SaveConfig = true, ConfigFolder = "icons"})
+local Credit = Window:MakeTab({
+	Name = "Credits",
+	Icon = "rbxassetid://4370318685",
+	PremiumOnly = false
+})
+Credit:AddButton({
+	Name = "Icons Made And Scripted By: Lucas!#5123",
+	Callback = function()
+      		setclipboard("https://discord.gg/lyc")
+              OrionLib:MakeNotification({
+                Name = "Space Hub",
+                Content = "Copied Invite Link!",
+                Image = "rbxassetid://4483345998",
+                Time = 5
+            })
+  	end    
+})
+
+
+local Tab = Window:MakeTab({
+	Name = "Icon1",
+	Icon = "rbxassetid://4370336019",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon2",
+	Icon = "rbxassetid://3944675151",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon3",
+	Icon = "rbxassetid://3605017115",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon4",
+	Icon = "rbxassetid://4483363527",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon5",
+	Icon = "rbxassetid://4400695581",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon6",
+	Icon = "rbxassetid://4370336704",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon7",
+	Icon = "rbxassetid://3944664684",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon8",
+	Icon = "rbxassetid://4450736564",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon9",
+	Icon = "rbxassetid://4483364237",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon10",
+	Icon = "rbxassetid://4483345278",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon11",
+	Icon = "rbxassetid://4370337241",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon12",
+	Icon = "rbxassetid://4483345463",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon13",
+	Icon = "rbxassetid://4335477481",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon14",
+	Icon = "rbxassetid://4400696294",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon15",
+	Icon = "rbxassetid://4384394779",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon16",
+	Icon = "rbxassetid://3944675664",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon17",
+	Icon = "rbxassetid://4400696929",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon18",
+	Icon = "rbxassetid://4400698359",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon19",
+	Icon = "rbxassetid://4400697855",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon20",
+	Icon = "rbxassetid://4400698963",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon21",
+	Icon = "rbxassetid://4330060040",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon22",
+	Icon = "rbxassetid://3605522284",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon23",
+	Icon = "rbxassetid://4483364408",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon24",
+	Icon = "rbxassetid://4483364327",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon25",
+	Icon = "rbxassetid://4400699701",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon26",
+	Icon = "rbxassetid://4483345607",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon27",
+	Icon = "rbxassetid://4384403999",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon28",
+	Icon = "rbxassetid://4370338095",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon29",
+	Icon = "rbxassetid://3944676352",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon30",
+	Icon = "rbxassetid://4384402413",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon31",
+	Icon = "rbxassetid://4384402990",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon32",
+	Icon = "rbxassetid://4384403532",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon33",
+	Icon = "rbxassetid://3610247188",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon34",
+	Icon = "rbxassetid://4483345737",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon35",
+	Icon = "rbxassetid://4483345875",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon36",
+	Icon = "rbxassetid://4335478275",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon37",
+	Icon = "rbxassetid://4483363084",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon38",
+	Icon = "rbxassetid://4384401919",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon39",
+	Icon = "rbxassetid://3944676934",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon40",
+	Icon = "rbxassetid://3944677737",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon41",
+	Icon = "rbxassetid://4483362748",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon42",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon43",
+	Icon = "rbxassetid://4483362458",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon44",
+	Icon = "rbxassetid://4400700509",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon45",
+	Icon = "rbxassetid://4483361337",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon46",
+	Icon = "rbxassetid://4483362299",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon47",
+	Icon = "rbxassetid://4400700924",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon48",
+	Icon = "rbxassetid://3944680095",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon49",
+	Icon = "rbxassetid://4384401360",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon50",
+	Icon = "rbxassetid://4370186570",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon51",
+	Icon = "rbxassetid://4384400812",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon52",
+	Icon = "rbxassetid://3944669799",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon53",
+	Icon = "rbxassetid://4335479121",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon54",
+	Icon = "rbxassetid://4335479658",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon55",
+	Icon = "rbxassetid://4400701343",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon56",
+	Icon = "rbxassetid://4335480353",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon57",
+	Icon = "rbxassetid://4335480896",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon58",
+	Icon = "rbxassetid://4370335364",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon59",
+	Icon = "rbxassetid://4370033185",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon60",
+	Icon = "rbxassetid://4335482118",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon61",
+	Icon = "rbxassetid://4370341699",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon62",
+	Icon = "rbxassetid://4370342507",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon63",
+	Icon = "rbxassetid://3944703587",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon64",
+	Icon = "rbxassetid://3944688398",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon65",
+	Icon = "rbxassetid://4483346149",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon66",
+	Icon = "rbxassetid://4370343755",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon67",
+	Icon = "rbxassetid://3610246221",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon68",
+	Icon = "rbxassetid://4384396122",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "🎉Icon69🎉",
+	Icon = "rbxassetid://4384396714",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon70",
+	Icon = "rbxassetid://4370344279",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon71",
+	Icon = "rbxassetid://4370344717",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon72",
+	Icon = "rbxassetid://4335482575",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon73",
+	Icon = "rbxassetid://4483346354",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon74",
+	Icon = "rbxassetid://4370345144",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon75",
+	Icon = "rbxassetid://3610248649",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon76",
+	Icon = "rbxassetid://4370345701",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon77",
+	Icon = "rbxassetid://4370316039",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon78",
+	Icon = "rbxassetid://4370316596",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon79",
+	Icon = "rbxassetid://3610245066",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon80",
+	Icon = "rbxassetid://4384400106",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon81",
+	Icon = "rbxassetid://4503342956",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon82",
+	Icon = "rbxassetid://3944689656",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon83",
+	Icon = "rbxassetid://3610239960",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon84",
+	Icon = "rbxassetid://3610242099",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon85",
+	Icon = "rbxassetid://4384394237",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon86",
+	Icon = "rbxassetid://4370318685",
+	PremiumOnly = false
+})
+
+
+local Tab = Window:MakeTab({
+	Name = "Icon87",
+	Icon = "rbxassetid://4370319235",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon88",
+	Icon = "rbxassetid://3944670656",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon89",
+	Icon = "rbxassetid://4370317928",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon90",
+	Icon = "rbxassetid://4335483334",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon91",
+	Icon = "rbxassetid://4384393547",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon92",
+	Icon = "rbxassetid://3605522284",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon93",
+	Icon = "rbxassetid://4335483762",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon94",
+	Icon = "rbxassetid://4400704299",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon95",
+	Icon = "rbxassetid://4370334869",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon96",
+	Icon = "rbxassetid://3610237052",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon97",
+	Icon = "rbxassetid://4506892591",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon98",
+	Icon = "rbxassetid://4335484343",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon99",
+	Icon = "rbxassetid://4384392959",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon100",
+	Icon = "rbxassetid://4377193226",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon101",
+	Icon = "rbxassetid://4384395384",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon102",
+	Icon = "rbxassetid://4384392464",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon103",
+	Icon = "rbxassetid://4370305054",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon104",
+	Icon = "rbxassetid://4370305588",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon105",
+	Icon = "rbxassetid://3944702361",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon106",
+	Icon = "rbxassetid://4335476290",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon107",
+	Icon = "rbxassetid://4370317406",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon108",
+	Icon = "rbxassetid://3944691398",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon109",
+	Icon = "rbxassetid://3944691867",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon110",
+	Icon = "rbxassetid://4370306254",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon111",
+	Icon = "rbxassetid://4384405947",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon112",
+	Icon = "rbxassetid://4384406773",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon113",
+	Icon = "rbxassetid://4335484884",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon114",
+	Icon = "rbxassetid://3605509925",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon115",
+	Icon = "rbxassetid://3944690667",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon116",
+	Icon = "rbxassetid://3605022185",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon117",
+	Icon = "rbxassetid://4384407160",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon118",
+	Icon = "rbxassetid://4384407582",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon119",
+	Icon = "rbxassetid://4384392025",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon120",
+	Icon = "rbxassetid://3944704135",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon121",
+	Icon = "rbxassetid://4370313618",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon122",
+	Icon = "rbxassetid://4370346095",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon123",
+	Icon = "rbxassetid://4400702457",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon124",
+	Icon = "rbxassetid://4400702947",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon125",
+	Icon = "rbxassetid://4370226511",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon126",
+	Icon = "rbxassetid://4506892784",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon127",
+	Icon = "rbxassetid://4335485422",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon128",
+	Icon = "rbxassetid://4335485957",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon129",
+	Icon = "rbxassetid://4335486524",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon130",
+	Icon = "rbxassetid://4335487169",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon131",
+	Icon = "rbxassetid://4370341699",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon132",
+	Icon = "rbxassetid://4335487866",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon133",
+	Icon = "rbxassetid://4335488543",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon134",
+	Icon = "rbxassetid://3944704985",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon135",
+	Icon = "rbxassetid://3944705374",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon136",
+	Icon = "rbxassetid://3944705939",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon137",
+	Icon = "rbxassetid://4335489011",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon138",
+	Icon = "rbxassetid://4370314188",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon139",
+	Icon = "rbxassetid://3944702835",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon140",
+	Icon = "rbxassetid://4400703447",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon141",
+	Icon = "rbxassetid://4400703888",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon142",
+	Icon = "rbxassetid://4370346582",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon143",
+	Icon = "rbxassetid://4400701828",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon144",
+	Icon = "rbxassetid://4335489513",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon145",
+	Icon = "rbxassetid://3610254229",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon146",
+	Icon = "rbxassetid://3610254425",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon147",
+	Icon = "rbxassetid://4400704837",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon148",
+	Icon = "rbxassetid://3944668821",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon149",
+	Icon = "rbxassetid://4384391488",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon150",
+	Icon = "rbxassetid://4370347078",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon151",
+	Icon = "rbxassetid://3610253578",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Icon152",
+	Icon = "rbxassetid://3610253853",
+	PremiumOnly = false
+})
+
+OrionLib:MakeNotification({
+	Name = "Space Hub",
+	Content = "Space Hub Icons Have Loaded",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
